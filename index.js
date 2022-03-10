1b4fb19344863a21df75dc9c6cc8b2e03d4b8602
ddderi <maxime830@live.fr>
2022-03-10 09:23:49 +0100

    commit tree f3f7e4e66115525646ef3b2852d4d29c51196b55

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+
+
+let createEmployeeRecord = function createEmployeeRecord([name, surname, titre, pay]){
+let employees = {
+    firstName: name,
+    familyName: surname,
+    title: titre,
+    payPerHour: pay,
+    timeInEvents: [],
+    timeOutEvents: []
+}
+ return employees
+}
+
+
+
+// function createEmployeeRecords(twoRows){
+//     let newarray = []
+//         for(let i=0;i<twoRows.length;i++){
+//            newarray.push(createEmployeeRecord(twoRows[i]))
+           
+//         }
+//         return newarray
+//         } 
+
+function createEmployeeRecords(array){
+   return array.map(element =>  createEmployeeRecord(element))
+    }
+
+
+
+function createTimeInEvent(createEmployeeRecord, date){
+    createEmployeeRecord.timeInEvents[0] = 'TimeIn'
+    createEmployeeRecord.timeInEvents[1] = 'dd'
+}
+
+
